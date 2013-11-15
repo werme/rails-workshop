6038538d4a04d11d05a61bd06b28b9d5b847567b
Olle Werme <olle.werme@gmail.com>
2013-11-15 13:45:57 +0100

    Add model specs

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131025150211) do
+ActiveRecord::Schema.define(version: 20131115095735) do
 
   create_table "categories", force: true do |t|
     t.string   "title"
@@ -27,5 +27,16 @@ ActiveRecord::Schema.define(version: 20131025150211) do
     t.integer  "rating"
     t.integer  "category_id"
   end
+
+  create_table "reviews", force: true do |t|
+    t.string   "title"
+    t.text     "body"
+    t.string   "author"
+    t.integer  "movie_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "reviews", ["movie_id"], name: "index_reviews_on_movie_id"
 
 end

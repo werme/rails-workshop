7d0cabee19dee08286ad3b8ef1d48cbbed67fe9a
Olle Werme <olle.werme@gmail.com>
2014-02-17 22:20:34 +0100

    Perfectionist

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,7 +1,7 @@
 class Movie < ActiveRecord::Base
   belongs_to :category
 
-  validates :title, presence: true, uniqueness: { :case_sensitive => false }, length: { in: 2..60 }
+  validates :title, presence: true, uniqueness: { case_sensitive: false }, length: { in: 2..60 }
   validates :category, presence: true
   validates :rating, inclusion: { in: 1..10, message: "%{value} is not in valid range 1 - 10." }
 end

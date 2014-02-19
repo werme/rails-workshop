b16f39597f4784a22c5c24580456cf0763cf0eac
Olle Werme <olle.werme@gmail.com>
2014-02-19 10:13:18 +0100

    Add methods to update movie info from omdb

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -4,5 +4,22 @@ class Movie < ActiveRecord::Base
   validates :title, presence: true, uniqueness: { case_sensitive: false }, length: { in: 2..60 }
   validates :category, presence: true
   validates :rating, inclusion: { in: 1..10, message: "%{value} is not in valid range 1 - 10." }
-end
 
+  before_save :update_info_from_omdb
+
+  def update_info_from_omdb
+    result = query_omdb(self.title)
+
+    self.year        = result["Year"]
+    self.description = result["Plot"]
+    self.poster_uri  = result["Poster"]
+  end
+
+  def query_omdb(query)
+    require 'open-uri'
+    require 'json'
+
+    uri = URI.parse(URI.encode("http://www.omdbapi.com/?t=#{query.strip}"))
+    result = JSON.parse(open(uri).read)
+  end
+end

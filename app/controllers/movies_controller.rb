338404690ad6bf66bb3be51a928ba3814340fcf6
Olle Werme <olle.werme@gmail.com>
2014-02-21 13:42:05 +0100

    Derpy educational comments

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,10 +1,15 @@
 class MoviesController < ApplicationController
   def list
     @movies = Movie.all
+
+    # Specify layout when neccessary.
+    # Defaults to app/views/layouts/application.html.erb.
     render layout: "landing"
   end
 
   def show
+    # Use decorators for presentational logic on the model. 
+    # Go check out the draper gem.
     @movie = Movie.find(params[:id]).decorate
   end
 
@@ -15,10 +20,15 @@ class MoviesController < ApplicationController
 
   def create
     @movie = Movie.new movie_params
+    
     if @movie.save
+      # Send flash messages to your views through the flash object
+      # or as arguments to redirect_to.
       flash[:alert] = "Tried to fetch additional information from omdb, please edit if incorrect." if @movie.year.present?
+      
       redirect_to movie_path(@movie), notice: "Movie was successfully created."
     else
+      # Use render over redirect to keep @movie with errors 
       render action: "new"
     end
   end
